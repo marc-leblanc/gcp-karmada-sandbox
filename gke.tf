9c0bd0f5ae79ab6f59ab5a68accbb5440fa4af23
Marc LeBlanc <marc.leblanc@arctiq.ca>
2021-05-01 09:30:20 -0400

    update linter

---
+++ gke.tf
@@ -0,0 +1,8 @@
+resource "google_container_cluster" "primary" {
+  count = var.karmada_gke_count
+  name     = "${var.gke_cluster_name}-${count.index}"
+  project  = var.gcp_project_id
+  location = var.project_zone
+  initial_node_count = 1
+  network  = var.karmada_network
+}
